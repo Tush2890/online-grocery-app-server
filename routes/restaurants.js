f41a799e9bab223afc04b7acb381b06330208b09
tush2890 <traj490@gmail.com>
2023-10-04 07:31:13 +0100

    modified the API to work for the search string

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,10 +2,17 @@ var express = require('express');
 const restaurants = require('../data/restaurants');
 var router = express.Router();
 
-/* GET restaurants listing based on location. */
-router.get('/:location', function (req, res, next) {
+router.get('/:location', (req, res, next) => {
   const location = req.params.location;
-  const filteresRestaurants = restaurants.filter(res => res.address.city === location);
+  const searchString = req.query.searchString?.toLocaleLowerCase();
+  let filteresRestaurants = [];
+  if (!searchString || searchString.length === 0) {
+    filteresRestaurants = restaurants.filter(res => res.address.city === location);
+  } else {
+    filteresRestaurants = restaurants.filter(res => res.address.city === location &&
+      (res.name.toLocaleLowerCase().includes(searchString) || res.category.some(cat => cat.includes(searchString)))
+    );
+  }
   res.json(filteresRestaurants).status(200);
 });
 
